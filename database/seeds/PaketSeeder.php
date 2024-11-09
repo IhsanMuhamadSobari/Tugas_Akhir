413e4f00453ac3b2690d3e178c57e1095a770549
IhsanMuhamadSobari <isan.ms35@gamil.com>
2024-11-09 20:02:54 +0700

    first commit

---
+++ database/seeds/PaketSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PaketSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('paket')->insert([
+            'id' => 1,
+            'ket' => 'Kelas 1',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('paket')->insert([
+            'id' => 2,
+            'ket' => 'Kelas 2',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('paket')->insert([
+            'id' => 3,
+            'ket' => 'Kelas 3',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('paket')->insert([
+            'id' => 4,
+            'ket' => 'Kelas 4',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('paket')->insert([
+            'id' => 5,
+            'ket' => "Kelas 5",
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('paket')->insert([
+            'id' => 6,
+            'ket' => 'kelas 6',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('paket')->insert([
+            'id' => 7,
+            'ket' => 'Kelas Khusus',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+    }
+}
